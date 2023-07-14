ee8f46b6b48310e611e908ec1e69690cddbd6c64
Abhemanyu Sarin <abhemanyus@gmail.com>
2023-07-14 20:45:00 +0530

    add tests

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -1,11 +1,15 @@
-import { Stage, StageProps } from "aws-cdk-lib";
+import { CfnOutput, Stage, StageProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { HelloCdkStack } from "./hello-cdk-stack";
 
 export class WorkshopPipelineStage extends Stage {
+  public readonly hcViewerUrl: CfnOutput;
+  public readonly hcEndpoint: CfnOutput;
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
-    new HelloCdkStack(this, "WebService");
+    const service = new HelloCdkStack(this, "WebService");
+    this.hcViewerUrl = service.hcViewerUrl;
+    this.hcEndpoint = service.hcEndpoint;
   }
 }
