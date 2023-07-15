431cfca435fee99fd322b9f8f1e2970048688a55
Abhemanyu Sarin <abhemanyus@gmail.com>
2023-07-15 08:06:01 +0530

    refactor for github actions

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { WorkshopPipelineStack } from "../lib/pipeline-stack";
+import { HelloCdkStack } from "../lib/hello-cdk-stack";
 
 const app = new cdk.App();
-new WorkshopPipelineStack(app, "CdkWorkshopPipelineStack", {
+new HelloCdkStack(app, "HelloCdkStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
