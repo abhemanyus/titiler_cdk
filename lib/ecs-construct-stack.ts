c7110f41053da31dc9b4e552d75c664950fc72ab
Abhemanyu Sarin <abhemanyus@gmail.com>
2023-07-15 16:46:09 +0530

    disable workflow, modify settings

--- lib/ecs-construct-stack.ts
+++ lib/ecs-construct-stack.ts
@@ -6,26 +6,28 @@ import { Construct } from "constructs";
 import {
   ApplicationLoadBalancer,
   ApplicationProtocol,
+  Protocol,
 } from "aws-cdk-lib/aws-elasticloadbalancingv2";
-import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
+// import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 
 const TITILER_IMAGE =
-  "ghcr.io/developmentseed/titiler@sha256:45565498b88d5b77360014ab9dd217c17617ae59d308769f67c24e7540a78d02";
-const CERTIFICATE_ARN =
-  "arn:aws:acm:ap-south-1:543620115200:certificate/d24695b7-936e-47eb-b435-d93b5d6c4ef1";
-
-export interface TitilerClusterProps {
-}
+"ghcr.io/developmentseed/titiler@sha256:45565498b88d5b77360014ab9dd217c17617ae59d308769f67c24e7540a78d02";
+// const TITILER_IMAGE = "amazon/amazon-ecs-sample";
+// const CERTIFICATE_ARN =
+"arn:aws:acm:ap-south-1:543620115200:certificate/d24695b7-936e-47eb-b435-d93b5d6c4ef1";
 
 export class TitilerCluster extends Construct {
   public readonly balancer: ApplicationLoadBalancer;
-  constructor(scope: Construct, id: string, props?: TitilerClusterProps) {
+  constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    const vpc = ec2.Vpc.fromLookup(this, "Vpc", {
+      isDefault: true,
+    });
+
     const cluster = new ecs.Cluster(this, "EcsCluster", {
-      vpc: ec2.Vpc.fromLookup(this, "Vpc", {
-        isDefault: true,
-      }),
+      vpc: vpc,
     });
 
     const ecsFargate = new ecs_patterns.ApplicationLoadBalancedFargateService(
@@ -40,19 +42,53 @@ export class TitilerCluster extends Construct {
           environment: {
             PORT: "80",
             WORKERS_PER_CORE: "1",
+            MAX_WORKERS: "1",
           },
-          // containerPort: 8000,
+          containerName: "titiler",
+          containerPort: 80,
+          enableLogging: true,
+          logDriver: ecs.LogDrivers.awsLogs({
+            streamPrefix: id,
+            logRetention: RetentionDays.ONE_WEEK,
+          }),
         },
         memoryLimitMiB: 2048,
         publicLoadBalancer: true,
+        listenerPort: 80,
+        runtimePlatform: {
+          operatingSystemFamily: ecs.OperatingSystemFamily.LINUX,
+          cpuArchitecture: ecs.CpuArchitecture.X86_64,
+        },
+        capacityProviderStrategies: [
+          {
+            capacityProvider: "FARGATE",
+            weight: 1,
+          },
+        ],
         // domainName: "cdk-cog.kesowa.com",
-        // protocol: ApplicationProtocol.HTTPS,
+        // protocol: ApplicationProtocol.HTTP,
         // certificate: Certificate.fromCertificateArn(
         //   this,
         //   "Certificate",
         //   CERTIFICATE_ARN,
         // ),
       },
+    );
+    ecsFargate.targetGroup.configureHealthCheck({
+      path: "/healthz",
+      // protocol: Protocol.HTTP,
+      // enabled: true,
+      // timeout: cdk.Duration.seconds(20),
+      // interval: cdk.Duration.seconds(40),
+      // unhealthyThresholdCount: 4,
+    });
+    ecsFargate.service.connections.allowToAnyIpv4(
+      new ec2.Port({
+        protocol: ec2.Protocol.ALL,
+        stringRepresentation: "All port 80",
+        fromPort: 80,
+      }),
+      "Allows traffic on port 80 from ALB",
     );
     this.balancer = ecsFargate.loadBalancer;
   }
